478cd279bbff1160010791d7d2a2150060ac18a9
Shalini Tiwar <salinitiwari12345@gmail.com>
2026-04-05 18:31:07 +0530

    buldings

--- components/ExperienceDifference.tsx
+++ components/ExperienceDifference.tsx
@@ -90,9 +90,16 @@ export default function ExperienceDifference() {
             }`}
           >
             <div className="absolute inset-0 bg-gradient-to-t from-gray-900/40 via-transparent to-transparent z-10 pointer-events-none"></div>
-            <Image
+            {/* <Image
               src="https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200&q=80"
               alt="Premium luxury modern building"
+              fill
+              className="object-cover transition-transform duration-[20s] ease-linear hover:scale-110"
+              sizes="(max-width: 1024px) 100vw, 50vw"
+            /> */}
+            <Image
+              src="/images/buldings.png"
+             alt="Premium luxury modern building"
               fill
               className="object-cover transition-transform duration-[20s] ease-linear hover:scale-110"
               sizes="(max-width: 1024px) 100vw, 50vw"
